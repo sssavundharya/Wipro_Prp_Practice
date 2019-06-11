43fec4c216bfd487fac9455928e09fc07d557c12
sssavundharya <36508650+sssavundharya@users.noreply.github.com>
2019-06-11 09:57:06 +0530

    Update OTPGeneration .java

--- OTPGeneration .java
+++ OTPGeneration .java
@@ -1,3 +1,17 @@
+/*		OTP GENERATION RULES
+1. The length of the OTP is exactly 8
+2. The OTP starts with first two characters of the account type (Case sensitive)
+3. The third character is first digit of the account number
+4. The fourth character is the sum of first five digits of account number (if the sum
+exceeds single digit then sum of digits should be done to form a single digit.
+Repeat if necessary)
+5. The fifth character is the first character of the account holder’s name
+6. The sixth character is the sum of sixth to tenth digit positions of the account
+number (if the sum exceeds single digit then sum of digits should be done to
+form a single digit. Repeat if necessary)
+7. The seventh character is the last character of the account holder’s name
+8. The eighth character is the last digit of the account number
+9. All characters should be same case of the input string*/
 import java.util.Scanner;
 
 public class OTPGeneration {
